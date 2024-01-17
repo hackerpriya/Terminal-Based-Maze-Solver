5b716b9390bae71b466516f8bf385c94d064f905
Priyanka <priyanka.bhrgva@gmail.com>
2024-01-17 20:13:03 +0530

    Finding-Path(BFS Algorithm)

--- maze_solver.py
+++ maze_solver.py
@@ -1,3 +1,4 @@
+from collections import deque
 import random
 from colorama import init, Fore
 
@@ -5,15 +6,18 @@ init(autoreset=True)
 
 # Maze Generation Function
 def generate_maze(n):
-    maze = [[random.choice(['◌', '▓']) for _ in range(n)] for _ in range(n)]
+    maze = [['◌' for _ in range(n)] for _ in range(n)]
     maze[0][0] = Fore.GREEN + 'S'  # Start
     maze[n - 1][n - 1] = Fore.BLUE + 'E'  # End
 
     # Mark a certain percentage of cells as walls
     num_walls = int(0.25 * n * n)
     for _ in range(num_walls):
-        row, col = random.randint(0, n - 1), random.randint(0, n - 1)
-        maze[row][col] = Fore.RED + '▓'
+        while True:
+            row, col = random.randint(0, n - 1), random.randint(0, n - 1)
+            if maze[row][col] != 'S' and maze[row][col] != 'E' and maze[row][col] != Fore.RED + '▓':
+                maze[row][col] = Fore.RED + '▓'
+                break
 
     return maze
 
@@ -25,18 +29,44 @@ def print_maze(maze):
 
 # Pathfinding Function (Using BFS)
 def find_path(maze):
-    # Implementation of BFS algorithm to find a path
-    pass
+    start = (0, 0)
+    end = (len(maze) - 1, len(maze[0]) - 1)
+
+    queue = deque([(start, [])])
+    visited = set()
+
+    while queue:
+        current, path = queue.popleft()
+        if current == end:
+            return path + [current]
+
+        if current in visited:
+            continue
+
+        visited.add(current)
+
+        row, col = current
+        neighbors = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
+
+        valid_neighbors = [(r, c) for r, c in neighbors if 0 <= r < len(maze) and 0 <= c < len(maze[0]) and maze[r][c] != Fore.RED + '▓' and (r, c) not in visited]
+
+        for neighbor in valid_neighbors:
+            queue.append((neighbor, path + [current]))
+
+    return None
 
 # Path Printing Function
-def print_path(path):
-    # Print the path in a readable format
+def print_path(path, maze):
     pass
+
 
 # Path Marking Function
 def mark_path(maze, path):
-    for row, col in path:
-        maze[row][col] = Fore.GREEN + '◍'
+    pass
+
+# Clearing Path Markings Function
+def clear_path(maze):
+    pass
 
 # Main Function
 def main():
@@ -51,11 +81,10 @@ def main():
         if option == '1':
             path = find_path(maze)
             if path:
-                print_path(path)
+                print_path(path, maze)
                 mark_path(maze, path)
-            else:
-                print(Fore.RED + "No path found.")
         elif option == '2':
+            clear_path(maze)
             maze = generate_maze(n)
         elif option == '3':
             print("Exiting the game.")
